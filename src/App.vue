99837734d8ac7ac8cda226bf322bd96151a21408
MezoAhmedII <mezoahmedamin@gmail.com>
2025-10-19 19:26:28 -0400

    blank app component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta explicabo ea quia illo! Eum aliquid illum vel animi
+    aperiam facere. Corrupti qui facilis animi tempore iste distinctio architecto id sint.</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
